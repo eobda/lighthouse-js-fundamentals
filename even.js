fd730347d2d480f3a15a7e54a5d08b77f2e62ecd
maddie <lee.maddie@gmail.com>
2023-07-12 09:35:08 -0400

    Added using value returned by function directly in another function vs in variable

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+// const tenIsEven = isEven(10); we can use function result directly where we need it
+// const elevenIsEven = isEven(11); we can use function result directly where we need it
+
+console.log(isEven(10));
+console.log(isEven(11));
