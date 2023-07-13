6904dd46d866d1d03eb78e41031a178cc9d5c535
maddie <lee.maddie@gmail.com>
2023-07-13 17:16:35 -0400

    Added test code for Ice Cream Arrays exercise.

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+// add root beer to the array of flavours
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// log the first flavour of the array
+console.log(iceCreamFlavours[0]);
+
+// log the last flavour of the array
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// log the total number of flavours
+console.log(iceCreamFlavours.length);
