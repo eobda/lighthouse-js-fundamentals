5c3d0a4dbaa4fc1d6ec5dd280c59e49af514249e
maddie <lee.maddie@gmail.com>
2023-07-24 09:52:37 -0400

    Write a function called whereCanIPark() that returns the coordinates of an available parking spot. Showing own work, not solution given on assignment page.

---
+++ katas/kata6.js
@@ -0,0 +1,98 @@
+/* Write a function called whereCanIPark() that returns the coordinates of an available parking spot for the vehicle, or returns false if there is no available spot. Our function receives an array of arrays representing parking spots, and a string with type of the vehicle that is looking for a parking spot.
+
+
+Regular cars can only park in R spots.
+Small cars can park in R or S spots.
+Motorcycles can park in R, S, or M spots.
+
+In the array of parking spots, spots are written in both lower-case and upper-case. An upper-case letter means that the particular spot is AVAILABLE, while lower-case letters mean that the spot is UNAVAILABLE.
+
+Our function must return an array with the coordinates of the spot as an [X, Y] pair. 
+
+There may be multiple available spots for a particular vehicle. It does not matter which spot your function chooses, as long as the spot is available. And if there are no available spots, return false.
+*/
+
+const whereCanIPark = function (spots, vehicle) {
+
+  let parking = [0, 0];
+  let available = false;
+
+  // loop over rows
+  for (let y = 0; y < spots.length; y++) {
+
+    // loop over columns
+    for (let x = 0; x < spots[y].length; x++) {
+      // if ((vehicle === 'small' && spots[y][x] === 'S') || (vehicle === 'small' && spots[y][x] === 'R') || (vehicle === 'regular' && spots[y][x] === 'R') || (vehicle === 'motorcycle' && spots[y][x] === 'S') || (vehicle === 'motorcycle' && spots[y][x] === 'R') || (vehicle === 'motorcycle' && spots[y][x] === 'M')) {
+      //   parking[0] = x;
+      //   parking[1] = y;
+      //   available = true;
+      // }
+
+      switch (vehicle) {
+        case 'small':
+          if (spots[y][x] === 'S' || spots[y][x] === 'R') {
+            parking[0] = x;
+            parking[1] = y;
+            available = true;
+          }
+          break;
+        case 'regular':
+          if (spots[y][x] === 'R') {
+            parking[0] = x;
+            parking[1] = y;
+            available = true;
+          }
+          break;
+        case 'motorcycle':
+          if (spots[y][x] === 'S' || spots[y][x] === 'R' || spots[y][x] === 'M') {
+            parking[0] = x;
+            parking[1] = y;
+            available = true;
+          }
+          break;
+      }
+    }
+  }
+
+  if (available) {
+    return parking;
+  } else {
+    return false;
+  }
+};
+
+console.log(whereCanIPark(
+  [
+    // COLUMNS ARE X
+    // 0    1    2    3    4    5
+    ['s', 's', 's', 'S', 'R', 'M'], // 0 ROWS ARE Y
+    ['s', 'M', 's', 'S', 'r', 'M'], // 1
+    ['s', 'M', 's', 'S', 'r', 'm'], // 2
+    ['S', 'r', 's', 'm', 'r', 'M'], // 3
+    ['S', 'r', 's', 'm', 'r', 'M'], // 4
+    ['S', 'r', 'S', 'M', 'M', 'S']  // 5
+  ],
+  'regular'
+)); // [4, 0]
+
+console.log(whereCanIPark(
+  [
+    ['M', 'M', 'M', 'M'],
+    ['M', 's', 'M', 'M'],
+    ['M', 'M', 'M', 'M'],
+    ['M', 'M', 'r', 'M']
+  ],
+  'small'
+)); // false
+
+console.log(whereCanIPark(
+  [
+    ['s', 's', 's', 's', 's', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['S', 'r', 's', 'm', 'r', 's'],
+    ['S', 'r', 's', 'm', 'R', 's'],
+    ['S', 'r', 'S', 'M', 'm', 'S']
+  ],
+  'motorcycle'
+)) // [3, 1]
