936d35e972361512ab497baca7d58fd920df33e2
maddie <lee.maddie@gmail.com>
2023-08-08 18:13:35 -0400

    Updated Loopy Lighthouse exercise to work with any given range, multiples, and words.

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,17 +1,31 @@
-for (let i = 100; i <= 200; i++) {
-  let print = "";
+/*
+Wrap the code you've written in a function called loopyLighthouse(range, multiples, words) that you call with 3 parameters.
 
-  if (i % 3 === 0) {
-    print += "Loopy";
-  }
-  
-  if (i % 4 === 0) {
-    print += "Lighthouse";
-  }
+range should be an array of 2 numbers representing the start and end values for the loop.
+multiples should be an array of 2 numbers representing the multiples you want to replace with words.
+words should be an array of 2 strings representing the words that will replace the multiples.
 
- if (i % 3 !== 0 && i % 4 !== 0) {
-    print = i;
-  }
+*/
 
-  console.log(print);
+function loopyLighthouse(range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    let print = "";
+
+    if (i % multiples[0] === 0) {
+      print += words[0];
+    }
+    
+    if (i % multiples[1] === 0) {
+      print += words[1];
+    }
+
+  if (i % multiples[0] !== 0 && i % multiples[1] !== 0) {
+      print = i;
+    }
+
+    console.log(print);
+  }
 }
+
+// test
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
