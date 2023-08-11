75bc10bca40affe60468124a07e44108b8da6f94
maddie <lee.maddie@gmail.com>
2023-08-11 10:50:59 -0400

    Create a function that converts strings to camel case.

--- katas/kata9.js
+++ katas/kata9.js
@@ -2,17 +2,18 @@
 
 const camelCase = function (input) {
   // split input
-  let output = input.split(' ');
+  let words = input.split(' ');
+  let output = words[0].toLowerCase();
+
   // for each input AFTER input[0], capitalize the first letter (input[x][0].toUpperCase())
-  if (output.length > 1) {
-    for (let i = 1; i < output.length; i++) {
-      let upper = output[i].charAt(0).toUpperCase();
-      output[i][0] = upper;
-      console.log(upper);
+
+  if (words.length > 1) {
+    for (let i = 1; i < words.length; i++) {
+      output = output + words[i][0].toUpperCase() + words[i].slice(1);
     }
   }
-  // join input with no spaces
-  return output.join('');
+
+  return output;
 };
 
 console.log(camelCase("this is a string")); // thisIsAString
