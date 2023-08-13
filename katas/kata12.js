f2035b3d531c95beed7a1d41a7d86c957b5e2c71
maddie <lee.maddie@gmail.com>
2023-08-13 19:12:10 -0400

    Write a function that helps determine which recipes match the ingredients that both bakeries have in stock.

---
+++ katas/kata12.js
@@ -0,0 +1,90 @@
+/*
+Let's write a function that helps determine which recipes match the
+ingredients that both bakeries have in stock.
+
+We need to complete a function called chooseRecipe(), which will receive three parameters:
+
+    An array of ingredients in stock at Bakery A
+    An array of ingredients in stock at Bakery B
+    An array of recipe objects. Each recipe has a name property(string)
+    and an ingredient property(array)
+
+We are limiting our search to two ingredient recipes. We want to find a
+recipe that utilizes one ingredient from Bakery A and one from Bakery B.
+
+Our chooseRecipe() function should return the name of the correct recipe.
+
+There will always be a single correct answer, and you will NOT need to
+consider special cases. For example, you do NOT need to worry about cases
+where one bakery has BOTH the ingredients for a recipe.
+
+We might want to start by creating a helper function called ingredientCheck()
+that will take in one bakery at a time, along with the recipes.ingredients
+array to check if the given bakery possesses any of the ingredients from
+that recipe.
+*/
+
+const ingredientCheck = function (bakery, ingredients) {
+  for (let ingredient of ingredients) {
+    for (let stock of bakery) {
+      if (ingredient === stock) {
+        return true;
+      }
+    }
+  }
+}
+
+const chooseRecipe = function (bakeryA, bakeryB, recipes) {
+  let name = '';
+
+  // go through recipes
+  for (let i = 0; i < recipes.length; i++) {
+    let checkA = ingredientCheck(bakeryA, recipes[i].ingredients);
+    let checkB = ingredientCheck(bakeryB, recipes[i].ingredients);
+    if (checkA && checkB) {
+      name = recipes[i].name;
+    }
+  }
+  // return name of recipe
+  return name;
+};
+
+let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
+let bakeryB = ["milk", "butter", "cream cheese"];
+let recipes = [
+  {
+    name: "Coconut Sponge Cake",
+    ingredients: ["coconut", "cake base"],
+  },
+  {
+    name: "Persian Cheesecake",
+    ingredients: ["saffron", "cream cheese"],
+  },
+  {
+    name: "Custard Surprise",
+    ingredients: ["custard", "ground beef"],
+  },
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+// Persian Cheesecake
+
+bakeryA = ["potatoes", "bay leaf", "raisins"];
+bakeryB = ["red bean", "dijon mustard", "apples"];
+recipes = [
+  {
+    name: "Potato Ganache",
+    ingredients: ["potatoes", "chocolate"],
+  },
+  {
+    name: "Sweet Fish",
+    ingredients: ["anchovies", "honey"],
+  },
+  {
+    name: "Nima's Famous Dijon Raisins",
+    ingredients: ["dijon mustard", "raisins"],
+  },
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+// Nima's Famous Dijon Raisins
