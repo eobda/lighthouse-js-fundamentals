03aae304d706dddcac10941d4e9f9fb267ce7115
maddie <lee.maddie@gmail.com>
2023-08-15 16:39:44 -0400

    Resolved issue where text was not splitting correctly

--- katas/kata18.js
+++ katas/kata18.js
@@ -14,66 +14,62 @@ const squareCode = function (message) {
   let output = '';
 
   // 2. find the square root of however long the message is and round up with Math.ceil(Math.sqrt(text.length))
-  let charsPerLine = Math.ceil(Math.sqrt(text.length));
+  let charsPerLine = Math.ceil(Math.sqrt(text.length));  
 
-
-  // 4. create new strings of whatever the square root is length
+  let splitArray = splitMessage(text, charsPerLine);
+  // 4. create new strings of charsPerLine length, using one character from each element of the split message array - NESTED LOOP
   // 5. return ONE STRING with spaces between the coded strings
+
+  output = codeText(splitArray, charsPerLine);
+
+
+  return output;
 };
 
 // 3. split the characters into strings of whatever the square root is length
 
-/* 
 const splitMessage = function (text, charsPerLine) {
   let output = [];
-  let x = 0;
-
-  for (let i = 0; i < text.length; i++) {
-    output[x] = '';
-
-    for (let n = 0; n < charsPerLine; n++) {
-      output[x] += text[i];
-    }
-    x++;
-  }
-
-  return output;
-}
-
-PROBLEM: this outputs the same character for the length of charsPerLine. I need i to move WHILE a counter counts to the length of charsPerLine and breaks it at that point (but i keeps moving).
-
-MAYBE: track if (i + 1) % charsPerLine === 0 and when it does, end the x?
-*/
-
-const splitMessage = function (text, charsPerLine) {
-  let output = [];
+  let string = '';
 
   for (let x = 0; x < text.length; x++) {
-    let string = '';
-
     // runs the loop through the whole text
 
     if ((x + 1) % charsPerLine === 0) {
       string = string + text[x];
+
+      // add current string to output, start "new" string
       output.push(string);
       string = '';
     } else {
       string = string + text[x];
-      console.log(string);
     }
 
+  }
+  // add final string to array
+  if (string !== '') {
+    output.push(string);
   }
 
   return output;
 }
 
-console.log(splitMessage("chillout", 3))
-// console.log(squareCode("chill out"));
-// console.log(squareCode("feed the dog"));
-// console.log(squareCode("have a nice day"));
-// console.log(
-//   squareCode("if man was meant to stay on the ground god would have given us roots")
-// );
+const codeText = function (textArray, charsPerLine) {
+  let output = textArray;
+
+  // 4. create new strings of charsPerLine length, using one character from each element of the split message array - NESTED LOOP
+  // 5. return ONE STRING with spaces between the coded strings
+
+
+  return output;
+}
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(
+  squareCode("if man was meant to stay on the ground god would have given us roots")
+);
 
 /*
 expected output:
